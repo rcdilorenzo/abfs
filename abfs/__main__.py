b3917db14a944cf0f0ab53643d13624ba7e35498
Christian Di Lorenzo <rcddeveloper@icloud.com>
2019-02-07 20:37:00 -0500

    Add more CLI options and attempt multi-gpu training

--- abfs/__main__.py
+++ abfs/__main__.py
@@ -16,14 +16,18 @@ def train(args):
     # Train (60%), Val (10%), Test (20%), Fixed seed
     split_config = DataSplitConfig(0.1, 0.2, 1337)
 
-    # Create data with batch size of 8
-    data = Data(config, split_config, batch_size=4)
+    # Create data with batch size
+    data = Data(config, split_config, batch_size=args.batch_size, augment=True)
 
     # Exclude empty polygons
     data.data_filter = lambda df: df.sq_ft > 0
 
     # Create model
-    unet = UNet(data, (512, 512), max_batches=80, epochs=500)
+    unet = UNet(data, (args.size, args.size),
+                max_batches=args.max_batches,
+                epochs=args.epochs,
+                gpu_count=args.gpu_count,
+                learning_rate=args.learning_rate)
 
     # Print summary
     unet.model.summary()
@@ -41,9 +45,22 @@ def function_named(name):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', type=str, choices=['train'])
+
+    subparsers = parser.add_subparsers()
+    train_parser = subparsers.add_parser('train', help='Train a neural network')
+    train_parser.add_argument('-lr', '--learning-rate', type=float, default=0.001)
+    train_parser.add_argument('-s', '--size', type=int, default=512,
+                              help='Size of image')
+    train_parser.add_argument('-e', '--epochs', type=int, default=100)
+    train_parser.add_argument('-b', '--batch-size', type=int, default=4,
+                              help='Number of examples per batch')
+    train_parser.add_argument('-mb', '--max-batches', type=int, default=999999,
+                              help='Maximum batches per epoch')
+    train_parser.add_argument('-gpus', '--gpu-count', type=int, default=1)
+    train_parser.set_defaults(func=train)
+
     args = parser.parse_args()
-    function_named(args.command)(args)
+    args.func(args)
 
 if __name__ == "__main__":
     main()
--- abfs/keras/u_net.py
+++ abfs/keras/u_net.py
@@ -10,31 +10,45 @@ from abfs.keras.generator import Generator
 from abfs.keras.custom_tensor_board import CustomTensorBoard
 
 import tensorflow as tf
+from keras.utils import multi_gpu_model
 from keras.models import *
 from keras.layers import *
-from keras.optimizers import *
+from keras.optimizers import Adam, SGD
 from keras.callbacks import ModelCheckpoint, TerminateOnNaN
 import keras.backend as K
 
 class UNet():
-    def __init__(self, data, shape, max_batches=math.inf, epochs=100):
+    def __init__(self, data, shape,
+                 max_batches=math.inf,
+                 gpu_count=1,
+                 epochs=100,
+                 learning_rate=0.001):
+
         self.uuid = uuid.uuid4().hex[0:6]
         self.data = data
         self.shape = shape
+        self.gpu_count = gpu_count
         self.max_batches = max_batches
         self.epochs = epochs
+        self.learning_rate = learning_rate
 
     def mean_iou(self, actual, pred):
         return abfs.keras.metrics.mean_iou(self.shape, actual, pred)
 
     def train(self):
-        self.model.compile(optimizer=Adam(lr=1e-4),
+        model = self.model
+
+        if self.gpu_count > 1:
+            model = multi_gpu_model(model, gpus=self.gpu_count, cpu_merge=False)
+
+        model.compile(optimizer=SGD(lr=self.learning_rate),
                            loss='binary_crossentropy',
                            metrics=[self.mean_iou])
 
-        self.model.fit_generator(self.train_generator,
-                                 validation_data=self.val_generator,
-                                 epochs=self.epochs, callbacks=self._callbacks())
+        model.fit_generator(self.train_generator,
+                            validation_data=self.val_generator,
+                            epochs=self.epochs,
+                            callbacks=self._callbacks())
 
     @property
     @memoize
@@ -70,20 +84,20 @@ class UNet():
             last_layer,
             UpSampling2D(size = (2,2)),
             Conv2D(filters, 2,
-                    activation = 'relu',
-                    padding = 'same',
-                    kernel_initializer = 'he_normal')
+                   activation = 'relu',
+                   padding = 'same',
+                   kernel_initializer = 'he_normal')
         )
         conv_up = pipe(
             concatenate([counterpart_layer, up], axis=3),
             Conv2D(filters, 3,
-                    activation = 'relu',
-                    padding = 'same',
-                    kernel_initializer = 'he_normal'),
+                   activation = 'relu',
+                   padding = 'same',
+                   kernel_initializer = 'he_normal'),
             Conv2D(filters, 3,
-                    activation = 'relu',
-                    padding = 'same',
-                    kernel_initializer = 'he_normal')
+                   activation = 'relu',
+                   padding = 'same',
+                   kernel_initializer = 'he_normal')
         )
         return conv_up
 
@@ -91,7 +105,6 @@ class UNet():
     @property
     @memoize
     def model(self):
-        # Model based on https://github.com/zhixuhao/unet implementation of U-Net
         height, width = self.shape
 
         inputs = Input((height, width, 3))
@@ -104,43 +117,28 @@ class UNet():
         conv2 = self.conv_block(128, 3, conv1_out)
         conv2_out = pipe(
             conv2,
-            MaxPooling2D(pool_size=(2, 2))
+            MaxPooling2D(pool_size=(2, 2)),
+            BatchNormalization()
         )
 
         conv3 = self.conv_block(256, 3, conv2_out)
-        conv3_out = pipe(
-            conv3,
-            MaxPooling2D(pool_size=(2, 2))
-        )
-
-        conv4 = self.conv_block(512, 3, conv3_out)
-        conv4_out = pipe(
-            conv4,
-            Dropout(0.5),
-            MaxPooling2D(pool_size=(2, 2))
-        )
-
-        conv5 = pipe(
-            conv4_out,
-            self.conv_block(1024, 3),
-            Dropout(0.5)
-        )
 
         up_side = pipe(
-            conv5,
-            self.conv_up_block(512, conv4),
-            self.conv_up_block(256, conv3),
+            conv3,
             self.conv_up_block(128, conv2),
             self.conv_up_block(64, conv1)
         )
 
         output = pipe(
             up_side,
-            Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'),
+            Conv2D(2, 3, activation = 'relu',
+                   padding = 'same',
+                   kernel_initializer = 'he_normal'),
             Conv2D(1, 1, activation = 'sigmoid')
         )
 
-        return Model(input=inputs, output=output)
+        with tf.device("/cpu:0"):
+            return Model(inputs=inputs, outputs=output)
 
     def _callbacks(self):
         model_format = 'unet-%s-{epoch:04d}-{val_loss:.2f}.hdf5' % self.uuid
@@ -149,6 +147,13 @@ class UNet():
         return [
             TerminateOnNaN(),
             CustomTensorBoard(log_dir, self.data, self.shape),
-            ModelCheckpoint('checkpoints/' + model_format,
+            MultiGPUCheckpoint('checkpoints/' + model_format,
                             save_best_only=True, period=1)
         ]
+
+class MultiGPUCheckpoint(ModelCheckpoint):
+    def set_model(self, model):
+        if isinstance(model.layers[-2], Model):
+            self.model = model.layers[-2]
+        else:
+            self.model = model
